b83e3642d08fcf44fca27308231a7f376839c3a9
Munir <info.mubir@gmail.com>
2021-04-28 21:44:21 +0900

    abc_mock

--- src/test/java/com/mubir/unittest/services/BusinessImplTestUsingSource.java
+++ src/test/java/com/mubir/unittest/services/BusinessImplTestUsingSource.java
@@ -3,19 +3,25 @@ package com.mubir.unittest.services;
 import com.mubir.unittest.domain.SomeSource;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+
 import static org.junit.jupiter.api.Assertions.*;
- class SomeSourceImpl implements SomeSource {
-     @Override
-     public int[] getAllData() {
-         return new int[]{4, 9, 2};
-     }
- }
-class BusinessImplTestUsingSource {
+import static org.mockito.Mockito.when;
+
+class BusinessImplTestUsingSourceMock {
 
     @Test
     void usingDataSource() {
         BusinessImpl business = new BusinessImpl();
-        business.setSomeSource(new SomeSourceImpl());
+        /*
+        * this is just creating SomeSource implementation using mockito
+        * other than creating class implementation we are using method
+        * level implementation.
+        * */
+        SomeSource source = mock(SomeSource.class);
+        when(source.getAllData()).thenReturn(new int[]{5,7,3});
+
+        business.setSomeSource(source);
         int actualResult = business.UsingDataSource();
         int expectedResult = 15;
         assertEquals(expectedResult,actualResult);
