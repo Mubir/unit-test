ef888ccc55d2cabd49ab580713a536e8e1870943
Munir <info.mubir@gmail.com>
2021-04-28 21:01:30 +0900

    abc

---
+++ src/test/java/com/mubir/unittest/BusinessImplTest.java
@@ -0,0 +1,16 @@
+package com.mubir.unittest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessImplTest {
+
+    @Test
+    void calculateSum() {
+        BusinessImpl business = new BusinessImpl();
+        int actualResult = business.calculateSum(new int[]{1,2,3,4});
+        int expectedResult = 10;
+        assertEquals(expectedResult,actualResult);
+    }
+}
